44f51a37d5b782c43dec5a231823a98648ca0c67
asoler <asoler@student.42sp.org.br>
2022-12-11 23:31:13 +0100

    atuliza variavel de ambiente PWD ao executar cd

--- builtins/cd.c
+++ builtins/cd.c
@@ -14,8 +14,20 @@
 
 void	builtin_cd(t_data *data, char *path)
 {
+	char	*join_pwd_path;
+	char	*current_path;
+
 	if (!path)
 		path = get_env_var(data, "HOME")->value;
 	if (chdir(path))
 		print_cmd_error(path, 0);
+	else
+	{
+		current_path = getcwd(0, 0);
+		join_pwd_path = ft_strjoin("PWD=", current_path);
+		builtin_export(data, join_pwd_path);
+		free(join_pwd_path);
+		free(current_path);
+	}
 }
+;
